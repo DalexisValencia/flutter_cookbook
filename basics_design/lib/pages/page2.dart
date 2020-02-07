65000daa72de8fab2b832de15e2f3b388e6acdf6
DalexisValencia <d.alexis.valencia@gmail.com>
2020-02-07 11:41:17 -0500

    Trabajar con tablas

--- basics_design/lib/pages/page2.dart
+++ basics_design/lib/pages/page2.dart
@@ -3,18 +3,50 @@ import 'package:flutter/material.dart';
 class PageDos extends StatelessWidget{
   @override
   Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("segunda pagina")
-      ),
-      body: Container(
+    return 
+        DefaultTabController(
+          length: 3,
+          child: Scaffold(
+            appBar: AppBar(
+              title: Text("segunda pagina"),
+              bottom: TabBar(
+                tabs: [
+                  Tab(icon: Icon(Icons.directions_car)),
+                  Tab(icon: Icon(Icons.directions_transit)),
+                  Tab(icon: Icon(Icons.directions_bike))
+                ]
+              ),
+            ),
+            body: TabBarView(
+              children: <Widget>[
+                FirstTabPage(),
+                Icon(Icons.directions_transit),
+                Icon(Icons.directions_bike)
+              ]
+            ),
+            floatingActionButton: FloatingActionButton(
+              child: Icon(Icons.play_arrow),
+              onPressed: (){
+                print('floating');
+              },
+            ),
+          ),
+        );
+  }
+}
+
+class FirstTabPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
         alignment: Alignment.center,
         child: Column(
           children: <Widget>[
+          
             RaisedButton(
               child: Text("Mostrar Snackbar"),
               onPressed: (){
-               final snackBar = SnackBar(
+              final snackBar = SnackBar(
                   content: Container(
                     padding: EdgeInsets.only(top:10, bottom: 10, left: 5),
                     child: Text("la info"),
@@ -33,13 +65,6 @@ class PageDos extends StatelessWidget{
             )
           ]
         )
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.play_arrow),
-        onPressed: (){
-          print('floating');
-        },
-      ),
-    );
+      );
   }
 }
