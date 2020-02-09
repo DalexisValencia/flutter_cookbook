9cf38da2d10e841c4d38698146dcae8d8cc93ad0
DalexisValencia <d.alexis.valencia@gmail.com>
2020-02-08 21:18:29 -0500

    Formularios

---
+++ forms/lib/pages/validation.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class ValidationForm extends StatefulWidget{
+  ValidationFormState createState() => ValidationFormState();
+}
+
+class ValidationFormState extends State<ValidationForm>{
+  final _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context){
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: <Widget>[
+          TextFormField(
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'please enter some text';
+              }
+              return null;
+            },
+          ),
+          RaisedButton(
+            onPressed: (){
+              if (_formKey.currentState.validate()){
+                Scaffold.of(context)
+                .showSnackBar(SnackBar(content: Text('Procesing Data')));
+              }
+            },
+            child: Text('Submit'),
+            )
+        ]
+      ),
+    );
+  }
+}
